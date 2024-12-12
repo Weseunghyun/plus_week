30dd6aacf2976fd1dfe0399ef0495f4081134571
Weseunghyun <bible091192@gmail.com>
2024-12-12 14:18:16 +0900

    refactor: reportUsers 메서드 DB 접근 최소화 JPQL을 사용, update 쿼리문을 직접 작성하여 DB의 조회, 변경 기능을 한번에 수행하도록 하여 DB로의 접근을 최소화하였다.

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,10 +1,19 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE User u SET u.status = 'BLOCKED' WHERE u.id IN :userIds")
+    int updateStatusToBlocked(@Param("userIds") List<Long> userIds);
 }
